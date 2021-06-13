9960a4cce3d68a7e5b635380dced409c307b23fc
Minstrel223 <eulerequation@126.com>
2021-06-13 18:30:56 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Electron构建的跨平台密码管理器
+# Electron构建的密码管理器
 初始文件key为空,保存文件的时候设置一个key，打开时输入这个key才能看到正确的内容
+采用简单的位运算加密算法
 ![](./screenshots/Screenshot_20200709_222629.png)
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,Menu } = require('electron')
 
-function createWindow () {   
+function createWindow () {
+  
+  // 隐藏菜单栏
+  Menu.setApplicationMenu(null)
   // 创建浏览器窗口
   const win = new BrowserWindow({
     width: 800,
@@ -11,7 +14,7 @@ function createWindow () {
   })
 
   // 并且为你的应用加载index.html
-  win.loadFile('index.html')
+  win.loadFile('./src/index.html')
 
   // 打开开发者工具
   //win.webContents.openDevTools()
